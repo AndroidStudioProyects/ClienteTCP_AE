14c24f7f064c87caed095fb2777d11b2d82f7097
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-02-23 20:27:38 -0300

    funcional

--- ClienteTCP_AE/app/src/main/java/com/example/diego/clientetcp_ae/MainActivity.java
+++ ClienteTCP_AE/app/src/main/java/com/example/diego/clientetcp_ae/MainActivity.java
@@ -1,19 +1,108 @@
 package com.example.diego.clientetcp_ae;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText Mensaje,edit_IP,edit_Puerto;
+    String IP;
+    int Puerto=9001;
+    Button Btn_Enviar;
+    TextView textServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-// se arego esto
+        LevantoXML();
+
+        Boton();
+
+
 
     }
 
+    private void Boton() {
 
+        Btn_Enviar.setOnClickListener(new View.OnClickListener() {
+
+            String IP= edit_IP.getText().toString();
+            String puerto=edit_Puerto.getText().toString();
+
+
+            @Override
+            public void onClick(View v) {
+                String mensaje1=Mensaje.getText().toString();
+                ClientAsyncTask clientAST;
+                clientAST  = new ClientAsyncTask();
+                clientAST.execute(new String[] { IP, puerto,mensaje1});
+
+                            }
+        });
+    }
+
+    private void LevantoXML() {
+
+        Btn_Enviar=(Button)findViewById(R.id.btn_Enviar);
+        textServer=(TextView)findViewById(R.id.textServer);
+        edit_IP=(EditText)findViewById(R.id.editIP);
+        edit_Puerto=(EditText)findViewById(R.id.editPuerto);
+        Mensaje=(EditText)findViewById(R.id.editMensaje);
+            }
+
+
+    class ClientAsyncTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... params) {
+            String result = null;
+            try {
+                //Create a client socket and define internet address and the port of the server
+                Socket socket = new Socket(params[0],Integer.parseInt(params[1]));
+                //Get the input stream of the client socket
+                InputStream is = socket.getInputStream();
+                //Get the output stream of the client socket
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                //Write data to the output stream of the client socket
+                out.println(params[2]);
+                //Buffer the data coming from the input stream
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(is));
+                //Read data in the input buffer
+                result = br.readLine();
+                //Close the client socket
+                socket.close();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            //Write server message to the text view
+            textServer.append("\n"+s);
+        }
+    }
 }
+
+
+
